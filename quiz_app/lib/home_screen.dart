796140277b2d054082306de6e6b15e60099447d8
Tarak Barjadiya <tarakbarjadiya@gmail.com>
2025-05-23 11:02:02 +0530

    Home Screen to Question Screen Navigation

--- quiz_app/lib/home_screen.dart
+++ quiz_app/lib/home_screen.dart
@@ -1,52 +1,56 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  const HomeScreen(this.switchScreen, {super.key});
+
+  final void Function() switchScreen;
 
   @override
   Widget build(context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Image.asset(
-          'assets/images/quiz-logo.png',
-          width: 250.0,
-          color: const Color.fromARGB(
-            150,
-            255,
-            255,
-            255,
-          ),
-        ),
-        const SizedBox(height: 30),
-        const Text(
-          "Flutter Quiz App",
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 20,
-          ),
-        ),
-        const SizedBox(height: 30),
-        OutlinedButton.icon(
-          onPressed: () {},
-          style: OutlinedButton.styleFrom(
-            foregroundColor: const Color.fromARGB(
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 250.0,
+            color: const Color.fromARGB(
+              150,
               255,
-              207,
-              205,
-              205,
-            ),
-            backgroundColor: const Color.fromARGB(
-              41,
-              145,
-              138,
-              138,
+              255,
+              255,
             ),
           ),
-          icon: const Icon(Icons.navigate_next),
-          label: const Text("Start Quiz"),
-        ),
-      ],
+          const SizedBox(height: 30),
+          const Text(
+            "Flutter Quiz App",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+            ),
+          ),
+          const SizedBox(height: 30),
+          OutlinedButton.icon(
+            onPressed: switchScreen,
+            style: OutlinedButton.styleFrom(
+              foregroundColor: const Color.fromARGB(
+                255,
+                207,
+                205,
+                205,
+              ),
+              backgroundColor: const Color.fromARGB(
+                41,
+                145,
+                138,
+                138,
+              ),
+            ),
+            icon: const Icon(Icons.navigate_next),
+            label: const Text("Start Quiz"),
+          ),
+        ],
+      ),
     );
   }
 }
--- quiz_app/lib/quiz.dart
+++ quiz_app/lib/quiz.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/gradiant_container.dart';
+import 'package:quiz_app/home_screen.dart';
+import 'package:quiz_app/questions_screen.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -9,15 +10,38 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  var activeScreen = 'start-screen';
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = 'questions-screen';
+    });
+  }
+
   @override
   Widget build(context) {
+    Widget screenWidget = HomeScreen(switchScreen);
+
+    if (activeScreen == 'questions-screen') {
+      screenWidget = const QuestionsScreen();
+    }
+    
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: GradiantContainer(const [
-          Color.fromARGB(255, 138, 24, 184),
-          Color.fromARGB(255, 115, 13, 146),
-        ]),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 138, 24, 184),
+                Color.fromARGB(255, 115, 13, 146),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: screenWidget,
+        ),
       ),
     );
   }
