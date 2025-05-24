bb87a55ee4c969443a0bd55eadbd2418e57c3577
Tarak Barjadiya <tarakbarjadiya@gmail.com>
2025-05-24 16:55:31 +0530

    Expense data model updated with dynamic icons

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -1,11 +1,18 @@
-import 'package:expense_tracker/expenses.dart';
+import 'package:expense_tracker/views/expenses_view.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(const ExpenseApp());
+}
+
+class ExpenseApp extends StatelessWidget {
+  const ExpenseApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Expenses(),
-    ),
-  );
+    );
+  }
 }
