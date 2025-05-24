85e95e878beb9287afd3b1eb17072a9fd2a7b944
Tarak Barjadiya <tarakbarjadiya@gmail.com>
2025-05-24 18:46:20 +0530

    New Expense Screen Layout

--- expense_tracker/lib/views/expenses_view.dart
+++ expense_tracker/lib/views/expenses_view.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/models/expense.dart';
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -25,12 +26,28 @@ class _ExpensesState extends State<Expenses> {
     ),
   ];
 
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => const NewExpense(),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("Expense Tracker"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: _openAddExpenseOverlay,
+          ),
+        ],
+      ),
       body: Column(
         children: [
-          const Text("Placeholder"),
+          const Text("Chart"),
           Expanded(
             child: ExpensesList(
               expenses: _registeredExpenses,
