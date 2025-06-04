f8f11c2927fffbf1ac4dcc0ae35fc3112d390aca
Tarak Barjadiya <tarakbarjadiya@gmail.com>
2025-06-04 14:55:43 +0530

    groceries_list_screen added

--- shopping_list/lib/screens/groceries_list_screen.dart
+++ shopping_list/lib/screens/groceries_list_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list/data/dummy_items.dart';
 
 class GroceriesListScreen extends StatelessWidget {
   const GroceriesListScreen({super.key});
@@ -7,8 +8,20 @@ class GroceriesListScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Your Groceries"),
+        title: const Text("Your Groceries"),
         toolbarHeight: 70,
+      ),
+      body: ListView.builder(
+        itemCount: groceryItems.length,
+        itemBuilder: (context, index) => ListTile(
+          leading: Container(
+            width: 24,
+            height: 24,
+            color: groceryItems[index].category.categoryColor,
+          ),
+          title: Text(groceryItems[index].name),
+          trailing: Text('${groceryItems[index].quantity}'),
+        ),
       ),
     );
   }
