484cd856f46ef026a84ceaf86873c52119128df9
Tarak Jain <tarakbarjadiya.com>
2025-06-10 12:25:40 +0530

    added LocalStorage with sqflite functionality - completed FavoritePlaces project

--- favorite_places/lib/screens/favorite_places_screen.dart
+++ favorite_places/lib/screens/favorite_places_screen.dart
@@ -4,11 +4,26 @@ import 'package:favorite_places/widgets/places_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class FavoritePlacesScreen extends ConsumerWidget {
+class FavoritePlacesScreen extends ConsumerStatefulWidget {
   const FavoritePlacesScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ConsumerStatefulWidget> createState() {
+    return _FavoritePlacesScreen();
+  }
+}
+
+class _FavoritePlacesScreen extends ConsumerState<FavoritePlacesScreen> {
+  late Future _futurePlaces;
+
+  @override
+  void initState() {
+    super.initState();
+    _futurePlaces = ref.read(userPlaceProvider.notifier).loadPlaces();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final userPlaces = ref.watch(userPlaceProvider);
 
     return Scaffold(
@@ -27,7 +42,14 @@ class FavoritePlacesScreen extends ConsumerWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: PlacesList(places: userPlaces),
+        child: FutureBuilder(
+          future: _futurePlaces,
+          builder: (context, asyncSnapshot) {
+            return asyncSnapshot.connectionState == ConnectionState.waiting
+                ? const Center(child: CircularProgressIndicator())
+                : PlacesList(places: userPlaces);
+          },
+        ),
       ),
     );
   }
